66284c191546e0443460a75167b6fb12123c0fa3
Mauricio Zanetti Filho <contact@thinkincoin.com>
2023-07-13 05:16:21 -0300

    update lists

--- src/buildList.js
+++ src/buildList.js
@@ -1,34 +1,43 @@
-const { version } = require('../package.json');
-const mainnet = require('./tokens/mainnet.json');
-const testnet = require('./tokens/testnet.json');
+const { version } = require("../package.json");
+const mainnet = require("./tokens/mainnet.json");
+const ropsten = require("./tokens/ropsten.json");
+const rinkeby = require("./tokens/rinkeby.json");
+const goerli = require("./tokens/goerli.json");
+const kovan = require("./tokens/kovan.json");
+const polygon = require("./tokens/polygon.json");
+const mumbai = require("./tokens/mumbai.json");
+const optimism = require("./tokens/optimism.json");
+const celo = require("./tokens/celo.json");
+const arbitrum = require("./tokens/arbitrum.json");
+const bnb = require("./tokens/bnb.json");
+const sepolia = require("./tokens/sepolia.json");
+const avalanche = require("./tokens/avalanche.json");
+const baseGoerli = require("./tokens/base-goerli.json");
+const baseGoerli = require("./tokens/harmony.json");
+
+const bridgeUtils = require('@uniswap/token-list-bridge-utils');
 
 module.exports = function buildList() {
-  const parsed = version.split('.');
-  return {
-    'name': 'Think in Coin Default List',
-    'timestamp': (new Date().toISOString()),
-    'version': {
-      'major': +parsed[ 0 ],
-      'minor': +parsed[ 1 ],
-      'patch': +parsed[ 2 ]
+  const parsed = version.split(".");
+  const l1List = {
+    name: "Think in Coin | Default List",
+    timestamp: new Date().toISOString(),
+    version: {
+      major: +parsed[0],
+      minor: +parsed[1],
+      patch: +parsed[2],
     },
-    'tags': {},
-    'logoURI': 'https://swoop-exchange.s3-us-west-1.amazonaws.com/swoop.png',
-    'keywords': [
-      'Think in coin',
-      'default',
-      'Token List'
-    ],
-    tokens: [
-      ...mainnet,
-      ...testnet
-    ]
+    tags: {},
+    logoURI: "ipfs://QmNa8mQkrNKp1WEEeGjFezDmDeodkWRevGFN8JCV7b4Xir",
+    keywords: ["uniswap", "Harmony", "default"],
+    tokens: [...mainnet, ...ropsten, ...goerli, ...kovan, ...rinkeby, ...polygon, ...mumbai, ...optimism, ...celo, ...arbitrum, ...bnb, ...sepolia, ...avalanche, ...baseGoerli, ...harmony]
       // sort them by symbol for easy readability
       .sort((t1, t2) => {
         if (t1.chainId === t2.chainId) {
           return t1.symbol.toLowerCase() < t2.symbol.toLowerCase() ? -1 : 1;
         }
         return t1.chainId < t2.chainId ? -1 : 1;
-      })
+      }),
   };
+  return bridgeUtils.chainify(l1List);
 };
